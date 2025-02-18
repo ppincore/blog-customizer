1727f46dae7f464ae9a1baf4c276c3d6545da33d
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-02-18 12:38:27 +0300

    add: sidebar open/close handle

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,21 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState, useRef } from 'react';
 
 export const ArticleParamsForm = () => {
+	const refAside = useRef<HTMLDivElement | null>(null);
+	const [isOpen, setIsOpen] = useState(false);
+	const handleToggleSideBar = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handleToggleSideBar} />
+			<aside
+				className={`${styles.container} ${isOpen ? styles.container_open : ''}`}
+				ref={refAside}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
